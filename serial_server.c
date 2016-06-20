80fb2eca7e5e3b43a234eb5c08b9239fa6feb320
lixingcong <lixingcong@live.com>
2016-06-21 00:38:58 +0800

    tcp sendall is in debuging

--- serial_server.c
+++ serial_server.c
@@ -1,17 +1,21 @@
-//Time-stamp: < serial_server.c 2016-06-20 23:31:00 >
+//Time-stamp: < serial_server.c 2016-06-21 00:36:15 >
 /*说明：串口端的接收数据，模拟串口
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>  
 #include <string.h>
+#include <sys/stat.h>  
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <time.h>
 
-#define MAX_BUFFER_LENTH 65530
+
+#define MAX_BUFFER_LENTH 65500
 #define ERROR printf
 #define LOGE printf
 /* 释放内存 */
@@ -23,9 +27,10 @@
 
 /* 封包用户输入的数据（来自串口），仿照shadowsocks-libev的封装包格式 */
 typedef struct user_content {
-	unsigned short index;
-	unsigned short data_size;
+	int index;
+	int data_size;
 	int sockfd;						/* sockdet文件描述符 */
+	int connected;				/* 是否已经连接，类似shadowsocks */
 	char *ip;
 	char *port;
 	char *data;
@@ -127,36 +132,80 @@ user_content_t *new_user_content_from_str(char *in){
 
 	return tmp;
 }
+
+/* 将user_content的内容全部发送出去 阻塞操作*/
+int sendall(int s, user_content_t *in){
+	int n;
+	while(in->index < in->data_size) {
+		n = send(s, in->data+in->index, 1300, 0);
+		printf("send %d bytes\n, now index is %d",n,in->index);//,in->data);
+		sleep(0.5);
+		if (n == -1) { printf("sendall error!\n");break; }
+		in->index += n;
+	}
+	return n==-1?-1:0; // 失敗時傳回 -1、成功時傳回 0
+}
 	
 #ifdef SERIAL_MAIN
 int main(int argc,char *argv[]){
-	char in[MAX_BUFFER_LENTH];
+	char in[MAX_BUFFER_LENTH * 20];
 	char *IP,*PORT;
 	/* 用户态的数据包格式ASCII */
 	user_content_t *my_content=NULL;
-	int sockfd_to_remote;
+	int sockfd_to_remote,filefd,byte_readed;
+
+	int status;
+	struct addrinfo hints, *res; /* 连接到remote */
 	
-	/* if (argc != 3) { */
-	/* 	fprintf(stderr,"usage: %s ip port\n",argv[0]); */
-	/* 	return 1; */
-	/* } */
+	if (argc != 3) {
+		fprintf(stderr,"usage: %s ip port\n",argv[0]);
+		return 1;
+	}
 	
-	/* IP=argv[1]; */
-	/* PORT=argv[2]; */
+	IP=argv[1];
+	PORT=argv[2];
 	
 	printf("hello\n");
 
-	if(read_input(in));
-	my_content=new_user_content_from_str(in);
+	/* if(read_input(in)); */
+	/* my_content=new_user_content_from_str(in); */
 
+	filefd=open("/tmp/dd.txt",O_RDWR);
+	byte_readed = read(filefd, in, sizeof(in)-2);
+	printf("%d bytes readed\n",byte_readed);
+	getchar();
+	in[byte_readed]=0;
+	
+	my_content=my_malloc(sizeof(user_content_t));
+	my_content->data_size=strlen(in);
+	my_content->index=0;
+	my_content->data=in;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC; // AF_INET 或 AF_INET6 可以指定版本
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // fill in my IP for me
+
+	/* if ((status = getaddrinfo(my_content->ip, my_content->port, &hints, &res)) != 0) { */
+	if ((status = getaddrinfo(IP, PORT, &hints, &res)) != 0) {
+		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+		return 1;
+	}
+	
 	/* 建立一個 socket： */
-	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	sockfd_to_remote = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
 	/* connect! */
-	if(status=(connect(sockfd, res->ai_addr, res->ai_addrlen))<0){
+	if(status=(connect(sockfd_to_remote, res->ai_addr, res->ai_addrlen))<0){
 		perror("connect error");
 		return 1;
 	}
+
+	printf("hello form \n");
+
+	if(-1==sendall(sockfd_to_remote,my_content))printf("sendall fail.\n");
+
+	
 	return 0;
 }
 #endif
